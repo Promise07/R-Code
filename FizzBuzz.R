27db8b4d16fef7597f6af5eb0c7a53adde8da55c
Konstantinos Sotirakos <kosta@sotirakos.com>
2025-06-05 23:13:56 -0400

    added comments

--- FizzBuzz.R
+++ FizzBuzz.R
@@ -1,15 +1,23 @@
+# Loop through numbers 1 to 50
+for (i in 1:50) {
 
-for (i in 1:50){
-    if (i%%5==0 && i%%3==0){
-        cat("fizz buzz\n")
+    # Check if the number is divisible by both 3 and 5
+    if (i %% 5 == 0 && i %% 3 == 0) {
+        cat("fizz buzz\n")  # Print "fizz buzz" if divisible by both
     }
-    else if (i%%5==0){
-        cat("buzz\n")
+
+    # Check if the number is divisible by 5 only
+    else if (i %% 5 == 0) {
+        cat("buzz\n")  # Print "buzz" if divisible by 5
     }
-    else if (i%%3==0){
-        cat("fizz\n")
+
+    # Check if the number is divisible by 3 only
+    else if (i %% 3 == 0) {
+        cat("fizz\n")  # Print "fizz" if divisible by 3
     }
-    else{
+
+    # If none of the above, print the number itself
+    else {
         cat(i, "\n")
     }
 }
