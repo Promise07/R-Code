22d06a4a2808a98b4125bcb4b6e0611f2a0a2a1a
Konstantinos Sotirakos <kosta@sotirakos.com>
2025-06-11 14:37:00 -0400

    basics for a window in R

---
+++ BigBoy.R
@@ -0,0 +1,7 @@
+library(shiny) #make sure you install.packages("shiny") in the R terminal
+
+ui <- fluidPage()
+
+server <- function(input, output) {}
+
+shinyApp(ui, server) #copy this into terminal and paste after playing this file if it doesn't open a file 
