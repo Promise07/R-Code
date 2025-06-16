98b805451e76108618be51ab39d844ca5d63c945
Konstantinos Sotirakos <kosta@sotirakos.com>
2025-06-15 21:56:23 -0400

    change of plans, made calculator

--- BigBoy.R
+++ BigBoy.R
@@ -1,7 +1,51 @@
-library(shiny) #make sure you install.packages("shiny") in the R terminal
+library(shiny)
 
-ui <- fluidPage()
+ui <- fluidPage(
+    titlePanel("Simple Calculator"),
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("num1", "Enter first number:", 0),
+            numericInput("num2", "Enter second number:", 0),
+            fluidRow(
+                column(3, actionButton("add", "Add")),
+                column(3, actionButton("subtract", "Subtract")),
+                column(3, actionButton("multiply", "Multiply")),
+                column(3, actionButton("divide", "Divide"))
+            )
+        ),
+        mainPanel(
+            h3("Result:"),
+            textOutput("result")
+        )
+    )
+)
 
-server <- function(input, output) {}
+server <- function(input, output, session) {
+    result <- reactiveVal(0)
 
-shinyApp(ui, server) #copy this into terminal and paste after playing this file if it doesn't open a file 
+    observeEvent(input$add, {
+        result(input$num1 + input$num2)
+    })
+
+    observeEvent(input$subtract, {
+        result(input$num1 - input$num2)
+    })
+
+    observeEvent(input$multiply, {
+        result(input$num1 * input$num2)
+    })
+
+    observeEvent(input$divide, {
+        if (input$num2 == 0) {
+            result("Error: Division by zero")
+        } else {
+            result(input$num1 / input$num2)
+        }
+    })
+
+    output$result <- renderText({
+        result()
+    })
+}
+
+shinyApp(ui, server)
