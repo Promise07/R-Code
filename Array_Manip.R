5bfdc1371e4e864f829ad9e8aa44149f4c3e23e2
Konstantinos Sotirakos <kosta@sotirakos.com>
2025-06-17 17:27:17 -0400

    Mmmmm vectors nice

---
+++ Array_Manip.R
@@ -0,0 +1,13 @@
+vector <- c(1,9,3,4,5)
+
+#Print second element or third index (R goes by element number not index number)
+print(vector[2])
+
+#Slicing just like python
+print(vector[2:4])
+
+#Adds value to every value in the vector
+print(vector+2)
+
+#filter for all values in vector greater than chosen value
+print(vector[vector>3])
